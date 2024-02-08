2f6145a08146523bed0c4b4c37b65a8aa86605df
Bultut-yegon <bultut9785@gmail.com>
2024-02-08 10:36:23 +0300

    second commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous">
+    </script>
+
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sign up</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <nav>
+        <h1 class="brand">KCB Loan Management System</h1>
+        <ul>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#">download pdf</a></li>
+            <li> <a href="#">updates</a></li>
+            <li> <a href="#">Track Loan</a></li>
+            <li> <a href="#" class="btn btn-success ">login</a></li>
+        </ul>
+    </nav>
+    <div class="section1">
+        <label class="imageText"> hello, welcome to KCB-LMS</label>
+        <img class="image" src="./images/kcb.png" alt="">
+
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="welcome" src="./images/kcb3.jpg" alt="">
+            </div>
+            <div class="col-md-8">
+                <h3>Welcome to KCB loan tracking management system</h3>
+                <p>This system will provide you with quality services for you to know all the details pertaining
+                    <span>loan
+                        management</span>
+                </p>
+                <p>The services include<br>1.Loan pdf guide<br>2.Real-time loan tracking<br>3.Generating monthly
+                    reports<br></p>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+
+</html>
