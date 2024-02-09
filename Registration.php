1373594aa0d7cb0efae9b428415b08b7802479f0
Bultut-yegon <bultut9785@gmail.com>
2024-02-09 12:14:07 +0300

    pull&push

---
+++ Registration.php
@@ -0,0 +1,46 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+
+<body>
+    <div>
+        <form action="Registration.php" method="POST">
+            <div>
+                <label for="name" class="lab">Name as per ID</label>
+                <input class="input-area" type="text" name="name">
+            </div>
+
+            <div>
+                <label for="name" class="lab">Phone Number</label>
+                <input class="input-area" type="tel" name="phone">
+            </div>
+
+            <div>
+                <label for="idNumber" class="lab">ID Number</label>
+                <input class="input-area" type="text" name="idNumber">
+            </div>
+
+            <div>
+                <label for="county" class="lab">County</label>
+                <input class="input-area" type="text" name="county">
+            </div>
+
+            <div>
+                <label for="dob" class="lab">Date of Birth</label>
+                <input class="input-area" type="calendar" name="dob">
+            </div>
+
+            <div>
+                <label for="name" class="lab">Name</label>
+                <input class="input-area" type="text" name="name">
+            </div>
+        </form>
+    </div>
+
+</body>
+
+</html>
